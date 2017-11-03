001f6d13f63230c2f2f9848214c214e54bad9427
Liam Drake <32195131+Drake-L-BPC-Cyan@users.noreply.github.com>
2017-11-03 09:24:50 +0000

    Add files via upload

---
+++ IF Statement.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$roll = echo rand(1,6)
+
+if $roll == 1:
+	echo "ALmost Snake Eyes!";
+elif $roll == 6:
+	echo "Winner!";
+else:
+	
+
+?>
+
+</body>
+</html> 
