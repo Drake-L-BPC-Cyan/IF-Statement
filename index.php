c1cacd60b2b1c1861becf111fde55e2412ee1679
Liam Drake <32195131+Drake-L-BPC-Cyan@users.noreply.github.com>
2017-11-03 09:39:45 +0000

    Update index.php

--- index.php
+++ index.php
@@ -6,12 +6,14 @@
 
 $roll = echo rand(1,6)
 
-if $roll == 1;
-	echo "ALmost Snake Eyes!";
-elif $roll == 6:
-	echo "Winner!";
-else:
-	end if;
+if ($roll == 1){
+echo "ALmost Snake Eyes!";
+}
+elif ($roll == 6){
+echo "Winner!";
+}
+else{
+}
 
 ?>
 
